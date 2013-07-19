85e908682108375b52e9501e356e53134de23816
sevenbits <sevenbits@sevenbits-H55M-D2H.(none)>
2013-07-19 15:14:14 +0700

    The new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drink::Application.config.secret_token = 'a82f680fa4f2732762ca8a60a09f26617891195cbc6a960efa51b6c66839ac64aaba5bac81994c225951a4447ba5deec0e02f9a10095ed0be78ef501c35d31c8'
